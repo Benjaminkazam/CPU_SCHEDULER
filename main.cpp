24a9708e73309f6a6a0d96f49f1c39d510fa7c80
Benjaminkazam <benjaminkazas@gmail.com>
2023-12-23 16:00:42 +0300

    #2

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+// here to implement job and process in struct
+struct Job{
+    int bursTime;
+    int arrivalTime;
+    int priority;
+};
+
+
