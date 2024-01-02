f8a8c7cc5a0be166227b81bf6479a2fb3578092d
Benjaminkazam <benjaminkazas@gmail.com>
2024-01-02 12:58:46 +0300

    first come, first served corrected

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <limits.h>
 
 using namespace std;
 
@@ -10,18 +11,19 @@ struct Job{
     int bursTime;
     int arrivalTime;
     int priority;
-    struct Job* next;
 };
 //this function is for read data from the text file
-struct Job* readInputFromFile(const char* fileName);
+int readInputFromFile(struct Job* jobs,const char* fileName);
 
 //this function is implemented for First come, First served;
-void fcomeFserved(struct Job* jobs, const char* fileName);
+void fcomeFserved(struct Job* jobs, const char* fileName, const char* outputFile);
 //function to display implement Round-Robin SCheduling
-void roundRobinScheduling (struct Job* jobs, int timeQuantum, const char *fileName);
-//void displayAverageWaitingTime(struct Job* jobs, char schedulingMethod);
+void roundRobinScheduling (struct Job* jobs, int timeQuantum, const char *fileName, const char* outputFile);
+//this function calculate and display average
+void displayAndCalculateAvg(const char* fileName, const char* methode,int waitingTime[],int n);
 //function to display the scheduling results
 void displayResults(const char* fileName);
+int countLines(const char* fileName);
 
 int main (int argc, char *argv[]){
     // i wanna check if the correct command line is provide
@@ -42,15 +44,18 @@ int main (int argc, char *argv[]){
             outputFile= argv[i+1];
         }
     }
-    // for read the input data from the file
-    struct Job* jobs= readInputFromFile(inputFile);
+    int size= countLines(inputFile);
+    struct Job jobs[size];
+
+    string preemptive ="off";
+    string methodScheduling= "None";
 
     // logic of the cpu scheduler(MENU PART)
  while(true){
     int selectionOption;
     cout<<" CPU SCHEDULER SIMULATOR \n";
-    cout<<"1. Scheduling Method(none)\n";
-    cout<<"2. Premptive Mode\n";
+    cout<<"1. Scheduling Method "<<"("<<methodScheduling<<")"<<"\n";
+    cout<<"2. Premptive Mode "<<"("<<preemptive<<")"<<"\n";
     cout<<"3. Show Result\n";
     cout<<"4. End Program\n";
     cout<<"Choose Option > ";
@@ -66,225 +71,116 @@ int main (int argc, char *argv[]){
             cout<<"3. Shortest Job First Scheduling\n";
             cout<<"4. Priority Scheduling\n";
             cout<<"5. Round Robin Scheduling\n";
-            cout<<" Option >";
+            cout<<" Option > ";
             
             cin>>SchedulingOption;
             
             switch (SchedulingOption)
             {
-             case 1: //first function
+             case 1: cout<<"None method Scheduling choosen\n"; 
+             methodScheduling= "None";//first function
              break;
-             case 2: fcomeFserved(jobs,outputFile);//Implement first come , first served function
+             case 2: fcomeFserved(jobs,inputFile,outputFile);
+             methodScheduling="First come, first  served";//Implement first come , first served function
              break;
              case 3: //third function
               break;
              case 4: //function
               break;
              case 5: int quantumValue;
-                        cout<<"\n enter the quantum value";
+                        cout<<"\n enter the quantum value ";
                         cin>> quantumValue;
-                        roundRobinScheduling(jobs,quantumValue,outputFile);       
+                        roundRobinScheduling(jobs,quantumValue,inputFile,outputFile);
+                        methodScheduling="Round Robin Scheduling ";       
              break;
              default:
-                cout<<"please make a good choice";
+                cout<<"please make a good choice \n";
               break;
             }
             break;
             case 2: //Preemptive Mode
+                if(preemptive== "off"){
+                    preemptive= "On";
+                    cout<<"the preemptive mode is on";
+                }
+                else{
+                    preemptive= "off";
+                    cout<< "the preemptive mode is off";
+                }
             break;
             case 3: displayResults(outputFile);//Show Res
             break;
-            case 4:cout<< "Exit the Program..\n\n";
+            case 4:
+            cout<<" All of Scheduling Resultts: \n";
+            displayResults(outputFile);
+            cout<< "Exit the Program..\n\n";
             exit(1);
             break;
             default: cout<< "Please make a valid selection.\n\n";
             break; 
         }
  }
-    // Free allocated memory for jobs
-
-    while(jobs!=NULL){
-        struct Job* temp = jobs;
-        jobs = jobs->next;
-        free(temp);
-    }
     return 0;
 
 }
 
-struct Job* readInputFromFile(const char* fileName){
+int readInputFromFile(struct Job* jobs,const char* fileName){
     FILE* inputFile= fopen(fileName, "r");
-    struct Job* head= NULL;
-    struct Job* tail= NULL;
+    
     if(!inputFile){
-        fprintf(stderr,"Error during opening file: %s\n", fileName);
-        return head;
+        cout<<"Error during opening file: "<<fileName<<"\n";
+        return 0;
     }
-
+    int n= 0;
     while(!feof(inputFile)){
-        struct Job* newJob = (struct Job*)malloc(sizeof(struct Job));
-        if(!newJob){
-            fprintf(stderr, "we didnt allocate the memory allocation\n");
-            return head;
-        }
-
-        char line[50];
-        if (fgets(line, sizeof(line), inputFile) == NULL) {
-            return head;
-        }
-
-        sscanf(line, "%d:%d:%d", &newJob->bursTime, &newJob->arrivalTime, &newJob->priority);
-        newJob->next = NULL;
-
-        if (!head) {
-            head = newJob;
-            tail = newJob;
-        } 
-        else {
-            tail->next = newJob;
-            tail = newJob;
-        }
+        fscanf(inputFile, "%d: %d:%d", &jobs[n].bursTime,&jobs[n].arrivalTime,&jobs[n].priority);
+        n++;
+       
     }
  fclose(inputFile);
- return head;
+ return n;
 }
 // function to implement first come, first served
 
-void fcomeFserved(struct Job* jobs, const char* fileName){
+void fcomeFserved(struct Job* jobs, const char* fileName, const char* outputFile){
     //Enter the ouptfile file
-    FILE*outputFile= fopen(fileName, "w");
-    if(!outputFile){
-        fprintf(stderr, "Error during opening output file: output.txt\n");
-        return;
-    }
-    //initialize variable for total waiting
-    int totalWaiting=0;
-    int currentT=0;
-    int i =0;
-    //go through linked list
-    struct Job* currentJob= jobs;
-    fprintf(outputFile, "SCheduling method: First come, First served");
-    fprintf(outputFile, "Process waiting time: \n");
-    while (currentJob!=NULL){
-        i++;
-        int waitingT= currentT- currentJob->arrivalTime;// calculate waiting time
-        if (waitingT<0){
-            waitingT=0;//just to be sure that waiting time is non negative
+    int n= readInputFromFile(jobs,fileName);
+    int totalTime=0, waitingTime[n]={0};
+    for(int i=0;i<n;i++){
+        if(totalTime>=jobs[i].arrivalTime){
+            waitingTime[i]=totalTime-jobs[i].arrivalTime;
         }
-
-        totalWaiting += waitingT;// update
-        //write proceess info in output file
-        fprintf(outputFile, "P%d: %d ms\n", i, waitingT);
-                
-        currentT += currentJob->bursTime;
-
-        currentJob=currentJob->next;
-
+        else{
+            waitingTime[i]=0;
+        }
+        totalTime+=jobs[i].bursTime;
     }
-    int nProcess=0;
-    struct Job* tempJob = jobs;
-    while (tempJob !=NULL){
-        nProcess++;
-        tempJob=tempJob->next;
-    }
+    cout<<"Scheduling Method: First come, First served\n Process waiting time: \n";
+    displayAndCalculateAvg(outputFile,"Scheduling Method: First come, First served\n Process waiting time: \n", waitingTime,n);
 
-    if (nProcess>0){
-        float avgWT=(float) totalWaiting/nProcess;
-        fprintf(outputFile,"Average waiting Time: %.1f ms\n",avgWT);
-    }
-    else{
-        cout<<"No process for calculation of average time";
-    }
-    fclose(outputFile);
-
+    
 }
 //function to implement Round-Robin scheduling 
-void roundRobinScheduling (struct Job* jobs, int timeQuantum, const char *fileName){
-    FILE *outputFile = fopen(fileName, "a");
-    if (!outputFile)
-    {
-        fprintf(stderr, "Error opening output file: %s\n", fileName);
-        return;
-    }
-
-    // Initialize variables for total waiting time and current time
-    int totalWaitingTime = 0;
-    int currentTime = 0;
-    int i = 0;
-
-    fprintf(outputFile, "Scheduling Method: Round Robin Scheduling – time_quantum=%d\n", timeQuantum);
-    fprintf(outputFile, "Process Waiting Times:\n");
-
-    // Traverse the linked list to process jobs in Round Robin order
-    struct Job *currentJob = jobs;
-    while (currentJob != NULL)
-    {
-        i++;
-
-        // Calculate waiting time for the current process
-        int waitingTime = currentTime - currentJob->arrivalTime;
-        if (waitingTime < 0)
-        {
-            waitingTime = 0; // Ensure waiting time is non-negative
-        }
-
-        // Update total waiting time
-        totalWaitingTime += waitingTime;
-
-        // Write process information to the output file
-        fprintf(outputFile, "P%d: %d ms\n", i, waitingTime);
-
-        // Update current time to the completion time of the current process
-        if (currentJob->bursTime > timeQuantum)
-        {
-            currentTime += timeQuantum;
-            currentJob->bursTime -= timeQuantum;
-            // Move the current job to the end of the queue
-            struct Job *temp = currentJob;
-            currentJob = currentJob->next;
-            temp->next = NULL;
-            // Append the job to the end of the linked list
-            struct Job *tempJob = jobs;
-            while (tempJob->next != NULL)
-            {
-                tempJob = tempJob->next;
-            }
-            tempJob->next = temp;
-        }
-        else
-        {
-            currentTime += currentJob->bursTime;
-            currentJob->bursTime = 0; // Process completed
-            // Move to the next job
-            currentJob = currentJob->next;
-        }
-    }
-
-    // Calculate and write average waiting time to the output file
-    int numberOfProcesses = 0;
-    struct Job *tempJob = jobs;
-    while (tempJob != NULL)
-    {
-        numberOfProcesses++;
-        tempJob = tempJob->next;
-    }
-
-    // Avoid division by zero
-    if (numberOfProcesses > 0)
-    {
-        float averageWaitingTime = (float)totalWaitingTime / numberOfProcesses;
-        fprintf(outputFile, "Average Waiting Time: %.1f ms\n", averageWaitingTime);
-    }
-    else
-    {
-        fprintf(outputFile, "No processes to calculate average waiting time.\n");
-    }
-
-    fclose(outputFile);
-
-
+void roundRobinScheduling (struct Job* jobs, int timeQuantum, const char *fileName, const char* outputFile){
 }
- 
+//function to count line into input file
+int countLines(const char* fileName){
+    FILE* file= fopen(fileName, "r");
+    if (file==NULL){
+        cout<<"Error opening file: "<<fileName;
+        return -1;
+    }
+    int lineCount= 0;
+    int cha; //number of character
+
+    while((cha= fgetc(file)) !=EOF){
+        if (cha == '\n'){
+            lineCount++;
+        }
+    }
+    fclose (file);
+    return lineCount;
+}
 
 // Function to display the scheduling results
 void displayResults(const char* fileName)
@@ -301,4 +197,25 @@ void displayResults(const char* fileName)
     }
 
     fclose(inputFile);
+}
+void displayAndCalculateAvg(const char* fileName, const char* methode,int waitingTime[],int n){
+    FILE* output= fopen(fileName,"a");
+    if(output==NULL){
+        cout<<"Error opening the output: " << fileName;
+        return;
+    }
+    fprintf(output,"%s",methode);
+    for (int i = 0; i < n; ++i){
+        cout<<"P"<<i +1<< ": "<<waitingTime[i]<< " ms\n";
+        fprintf(output, "P%d: %d ms\n", i+1, waitingTime[i]);
+    }
+    int totalWaitingTime=0;
+    for (int i=0; i<n; i++){
+        totalWaitingTime+= waitingTime[i];
+    }
+    double average = totalWaitingTime / n;
+    cout<< "Average waiting time: "<< average<< " ms\n";
+    fprintf(output, "Average waiting time: %.1f ms\n", average);
+
+    fclose(output);
 }
